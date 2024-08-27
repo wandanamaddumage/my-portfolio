41e75d37fd55451642966400d61b7f993f4ee1d8
Wandana Mddumage <wandanamuthumali99@gmail.com>
2024-08-27 11:28:38 +0530

    feat: add init commit

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed p-5 mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+    <nav className="fixed px-10 mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/wandanamaddumage">
