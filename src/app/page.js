74fbb9e99f32243b183e3dcd2367cdfd6925a30d
Wandana Mddumage <wandanamuthumali99@gmail.com>
2024-08-27 10:01:45 +0530

    feat: add init commit

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ export default function Home() {
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         {/* <AchievementsSection /> */}
+
         <AboutSection />
         <ProjectsSection />
         <EmailSection />
