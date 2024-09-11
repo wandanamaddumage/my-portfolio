6154766d06bd634337b416cc2fb6c5294a5769ca
Wandana Mddumage <wandanamuthumali99@gmail.com>
2024-09-11 16:57:36 +0530

    feat: update the cv link

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -39,7 +39,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/drive/u/1/folders/1zpkhXoSS5KB8SUkhuJL4_78KODNvOwCp"
+              href="https://drive.google.com/file/d/1zBlLwWKZ-vgWferrhLxpHEtPfEMGFsVh/view?usp=drive_link"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
